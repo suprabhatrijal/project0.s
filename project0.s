31ae58e9a21deb803f765e7ab276b969c6300f8b
Suprabhat Rijal <goodguysman@gmail.com>
2022-10-06 15:35:40 -0400

    Offset each character by 1 to print second line

--- project0.s
+++ project0.s
@@ -32,7 +32,37 @@ lb $t1, 5($t0) # t2 has 0
 sb $t2, 5($t0) # word is 000@03060 
 sb $t1, 8($t0) # word is 000@03060
 
-
 li $v0, 4
 la $a0, id
 syscall
+
+li $s1, 0 # start of the counter
+li $s2, 8 # end of the counter
+
+# offset each character by 1
+lb $t1, 0($t0)  
+lb $t2, 8($t0)  
+sb $t1, 8($t0) 
+lb $t1, 7($t0) 
+sb $t2, 7($t0) 
+lb $t2, 6($t0) 
+sb $t1, 6($t0) 
+lb $t1, 5($t0) 
+sb $t2, 5($t0)
+lb $t2, 4($t0) 
+sb $t1, 4($t0) 
+lb $t1, 3($t0) 
+sb $t2, 3($t0) 
+lb $t2, 2($t0) 
+sb $t1, 2($t0)
+lb $t1, 1($t0)
+sb $t2, 1($t0) 
+lb $t2, 0($t0) 
+sb $t1, 0($t0) 
+li $v0, 4
+syscall
+
+li $v0, 10
+syscall
+
+
